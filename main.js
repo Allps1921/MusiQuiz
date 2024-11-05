956b018953430482ed5b0631be86bba9d7ce653d
Allan Lima <141273699+Allps1921@users.noreply.github.com>
2024-11-05 13:26:24 -0300

    Adição de javascript
    Adição de label para adicionar nickname.
    
    Adição de controle de eventos para start game
    
    adição e controle da classe hide

--- main.js
+++ main.js
@@ -1,8 +1,49 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const genreButton = document.querySelectorAll('.genre-button');
-  const startButton = document.getElementById('start-button');
-  const tittleButton = document.getElementById('tittle-button');
-}
-  genreButton.addEventListener("click", genreButtonSelect)
-  startButton.addEventListener('click', startGameQuiz)
-  tittleButton.addEventListener('click', tittleButtonSelect)
+document.addEventListener('DOMContentLoaded', () => {
+  const startButton = document.querySelector('.start-game')
+  const genreButtonContainer = document.querySelector('.genre-button')
+  const questionContainer = document.querySelector('.question-container')
+  const answerContainer = document.querySelector('.answer-container')
+  const nickNameContainer = document.querySelector('.username')
+  const advanceButton = document.querySelector('.enter-button')
+  let nickName
+  let questions = [];
+  let currentQuestionIndex = 0;
+  let score = 50000
+  let timer
+
+  // start game
+  startButton.addEventListener('click', () => {
+    nickNameContainer.classList.remove('hide')
+    advanceButton.classList.remove('hide')
+    startButton.classList.add('hide')
+    nickNameContainer.focus
+
+    // coletar dados ao pressionar enter ou clicar no botão avançar
+    advanceButton.addEventListener('click', handleAdvance)
+    nickNameContainer.addEventListener('keypress', (e) => {
+      // retorna a mesma função que adance button se pressionado a tecla enter
+      if (e.key === 'Enter') {
+        handleAdvance()
+      }
+    })
+
+    function handleAdvance() {
+      nickName = nickNameContainer.value.trim()
+      if (nickName) {
+        nickNameContainer.classList.add('hide')
+        advanceButton.classList.add('hide')
+        genreButtonContainer.classList.add('hide')
+      } else {
+        alert('Please, insert a nickName')
+      }
+    }
+  }
+  )
+
+
+
+})
+
+
+
+
